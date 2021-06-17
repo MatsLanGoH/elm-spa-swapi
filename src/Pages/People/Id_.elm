ed0b110d58b48246f6ac196536e78d7cd14fb1e0
MatsLanGoH <mats.lan.pod@googlemail.com>
2021-06-17 04:41:27 +0000

    Initial info for person retrieval

--- src/Pages/People/Id_.elm
+++ src/Pages/People/Id_.elm
@@ -1,34 +1,78 @@
-module Pages.People.Id_ exposing (page)
+module Pages.People.Id_ exposing (Model, Msg, page)
 
+import Api.Data exposing (Data)
+import Api.Person exposing (Person)
+import Gen.Params.People.Id_ exposing (Params)
 import Html exposing (Html)
-import Page exposing (Page)
-import Request exposing (Request)
+import Html.Attributes as Attr
+import Page
+import Request
 import Shared
 import UI
 import View exposing (View)
 
 
-type alias Params =
-    { id : String
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page shared req =
+    Page.element
+        { init = init req.params
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { id : String }
+
+
+init : Params -> ( Model, Cmd Msg )
+init params =
+    ( { id = params.id }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Person Detail " ++ model.id
+    , body = UI.layout <| viewBody model
     }
 
 
-page : Shared.Model -> Request.With Params -> Page
-page _ req =
-    Page.static
-        { view = view req.params }
-
-
-view : Params -> View msg
-view params =
-    { title = "People Detail " ++ params.id
-    , body = UI.layout <| viewBody params
-    }
-
-
-viewBody : Params -> List (Html msg)
-viewBody params =
-    [ Html.h1 [] [ Html.text ("People detail: " ++ params.id) ]
+viewBody : Model -> List (Html msg)
+viewBody model =
+    [ Html.h1 [] [ Html.text ("People detail: " ++ model.id) ]
     , Html.div []
         [ Html.p []
             [ Html.text "People details will go here" ]
