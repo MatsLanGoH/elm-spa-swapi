0f1a7bc97fd6aa6feab7a9d0783f8efe1f5a71d2
MatsLanGoH <mats.lan.pod@googlemail.com>
2021-06-17 00:00:21 +0000

    start implementing http/json decoding
    for peopleindex

--- src/Api/PeopleIndex.elm
+++ src/Api/PeopleIndex.elm
@@ -1,5 +1,9 @@
 module Api.PeopleIndex exposing (PeopleIndex)
 
+import Effect exposing (Effect)
+import Json.Decode as Json
+import Utils.Json exposing (withField)
+
 
 type alias PeopleIndex =
     { uid : String
@@ -7,3 +11,25 @@ type alias PeopleIndex =
 
     -- , url : String
     }
+
+
+decoder : Json.Decoder PeopleIndex
+decoder =
+    Utils.Json.record PeopleIndex
+        |> withField "uid" Json.string
+        |> withField "name" Json.string
+
+
+
+-- |> withField "url" Json.string
+
+
+type alias Listing =
+    { peopleIndices : List PeopleIndex
+    }
+
+
+paginatedDecoder : Json.Decoder Listing
+paginatedDecoder =
+    Json.map Listing
+        (Json.field "results" (Json.list decoder))
---
+++ src/Utils/Json.elm
@@ -0,0 +1,31 @@
+module Utils.Json exposing (record, withDefault, withField)
+
+import Json.Decode as Json
+
+
+
+-- DECODING RECORDS
+
+
+record : fn -> Json.Decoder fn
+record =
+    Json.succeed
+
+
+withField : String -> Json.Decoder field -> Json.Decoder (field -> value) -> Json.Decoder value
+withField str decoder =
+    apply (Json.field str decoder)
+
+
+withDefault : value -> Json.Decoder value -> Json.Decoder value
+withDefault fallback decoder =
+    Json.maybe decoder |> Json.map (Maybe.withDefault fallback)
+
+
+
+-- INTERNALS
+
+
+apply : Json.Decoder field -> Json.Decoder (field -> value) -> Json.Decoder value
+apply =
+    Json.map2 (|>)
